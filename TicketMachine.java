0d6db172353bcf15ef4ce6d7f58d307a10b36df7
Prasanthi Penmadu <prasanthipenmadu@zipcodersMBP10.fios-router.home>
2019-05-22 22:02:49 -0400

    Updates

--- TicketMachine.java
+++ TicketMachine.java
@@ -5,10 +5,7 @@
  * It is a naive machine in the sense that it trusts its users
  * to insert enough money before trying to print a ticket.
  * It also assumes that users enter sensible amounts.
- *
- * @author David J. Barnes and Michael Kolling
- * @version 2008.03.30
- */
+ *Users have an option of two tickets to buy at different prices*/
   class TicketMachine
 {
     // The price of a ticket from this machine.
@@ -16,42 +13,36 @@
     // The amount of money entered by a customer so far.
     private int balance;
     // The total amount of money collected by this machine.
-    private int total;
+    private int price2;
 
-    /**
-     * Create a machine that issues tickets of the given price.
-     * Note that the price must be greater than zero, and there
-     * are no checks to ensure this.
-     */
-    public TicketMachine(int ticketPrice )
+    
+    public TicketMachine(int ticketPrice1,int ticketPrice2 )
     {
-        price = ticketPrice;
+        price = ticketPrice1;
+        price2=ticketPrice2;
         balance = 0;
-        total = 0;
+        
     }
     
 
-    /**
-     * Return the price of a ticket.
-     */
+    
     public int getPrice()
     {
         return price;
     }
+    public int getPrice2()
+    {
+        return price2;
+    }
 
-    /**
-     * Return the amount of money already inserted for the
-     * next ticket.
-     */
+    
     public int getBalance()
     {
         return balance;
     }
    
 
-    /**
-     * Receive an amount of money in cents from a customer.
-     */
+    
     public void insertMoney(int amount)
     {
         if(amount>0)
@@ -59,15 +50,8 @@
         else
         System.out.println("Invalid amount");
     }
-    public void showPrice(){
-     System.out.println("The price of a ticket is "+price+"cents.");
-    }
+   
 
-    /**
-     * Print a ticket.
-     * Update the total collected and
-     * reduce the balance to zero.
-     */
     public void printTicket()
     {   
         if(balance>=price){
@@ -83,9 +67,22 @@
     else{
      System.out.println("You still need to pay "+(price-balance));
     }
-        // Update the total collected with the balance.
+    
+    }
+     public void printTicket2(){
       
-        // Clear the balance.
+    if(balance>=price2){
+        // Simulate the printing of a ticket.
+        System.out.println("##################");
+        System.out.println("# The BlueJ Line");
+        System.out.println("# Ticket");
+        System.out.println("# " + price2 + " cents.");
+        System.out.println("##################");
+        System.out.println();
+        balance =balance-price2;
+    }else{
+     System.out.println("You still need to pay "+(price2-balance));  
+        }
         
     }
 }
